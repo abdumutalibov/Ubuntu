cd1b90e811bc28b737b980e91e37980470bdeedd
abdumutalibov <azimabdumutalibov21@gmail.com>
2021-11-10 22:44:04 +0500

    azim

---
+++ main.js
@@ -0,0 +1,13 @@
+let arr =0
+let arrNew =0
+for(let i=0;i<=100; i++){
+    if(i%2==0){
+arr+=i
+    }
+}
+for(let j=0;j<100;j++){
+    if (j%2==1) {
+       arrNew+=j 
+    }
+}
+console.log(arr+ arrNew);
